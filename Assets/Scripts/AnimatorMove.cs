f18a4c196fceebd6d9cfa59ebaf5fe3ca6ef988d
RomanAstra <roman_muratov_astu@mail.ru>
2020-04-20 22:20:32 +0300

    Initial commit

---
+++ Assets/Scripts/AnimatorMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public sealed class AnimatorMove : MonoBehaviour
+{
+    private Animator _animator;
+
+    private static readonly int _animatorRunningSpeed = Animator.StringToHash("RunningSpeed");
+    private static readonly int _animatorJumpTrigger = Animator.StringToHash("Jump");
+
+    private void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Move(float value)
+    {
+        _animator.SetFloat(_animatorRunningSpeed, value);
+    }
+
+    public void Jump()
+    {            
+        _animator.SetTrigger(_animatorJumpTrigger);
+
+    }
+}
